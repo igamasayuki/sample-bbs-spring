186fa4b7d684c467cd68661189a3ddb9e6d6b2a7
Masayuki IGA <igamasayuki@gmail.com>
2018-06-09 06:06:09 +0900

    演習用にデプロイ用の設定を元に戻しました

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,18 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer {
+public class App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 	
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(App.class);
-    }
 }
