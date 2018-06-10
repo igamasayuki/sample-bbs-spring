7f25b2f8178c62817797b1cabc7f782adf858e14
Masayuki IGA <igamasayuki@gmail.com>
2018-06-11 08:55:56 +0900

    不要なimport文を削除

--- src/main/java/com/example/joined/controller/JoinedBbsController.java
+++ src/main/java/com/example/joined/controller/JoinedBbsController.java
@@ -13,7 +13,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.joined.domain.JoinedArticle;
 import com.example.joined.domain.JoinedComment;
